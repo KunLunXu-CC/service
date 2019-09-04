d4a8df4bca4cdb15f9d8a70b97b85e3c89baf37f
qianyin925 <qianyin95@gmail.com>
2019-09-04 17:25:25 +0800

    refactor: 项目部署

--- app/middleware/cross.js
+++ app/middleware/cross.js
@@ -1,12 +1,16 @@
+const cors = require('koa2-cors');
+ 
 // 跨域设置
-module.exports = async (ctx, next) => {
-  const allowHeaders = [
-    'Authorization',
-    'Content-Type',
-  ];
-  // 允许响应返回的 header 字段
-  ctx.set('Access-Control-Allow-Headers', allowHeaders.join(', '));
-  // 响应头中，允许浏览器获取的字段
-  ctx.set('Access-Control-Expose-Headers', allowHeaders.join(', '));
-  await next();
-}
+module.exports = cors({
+  origin: function(ctx) {
+    if (ctx.url === '/test') {
+      return false;
+    }
+    return '*';
+  },
+  maxAge: 5,
+  credentials: true,
+  allowMethods: ['GET', 'POST'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  exposeHeaders: ['Content-Type', 'Authorization'],
+});
