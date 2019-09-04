66d655c3285f6065b6176f1a44dc916353dc1684
qianyin925 <qianyin95@gmail.com>
2019-09-04 14:32:13 +0800

    refactor: docker 项目部署测试

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,6 @@ module.exports = {
     name: 'app',
     script: './app/app.js',
     watch: true,
-    ignore_watch: ['node_modules', 'docker']
-    env: {
-      NODE_ENV: 'production'
-    }
+    ignore_watch: ['node_modules', 'docker'],
   }]
 }
