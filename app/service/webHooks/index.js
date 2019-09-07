b5a49bf0b0aba5bfa8965b815614cde4d093da0d
qianyin925 <qianyin95@qq.com>
2019-09-07 16:31:39 +0800

    refactor: webhooks 测试

--- app/service/webHooks/index.js
+++ app/service/webHooks/index.js
@@ -1,16 +1,35 @@
 const fs = require('fs');
 const path = require('path');
-const { hmac } = require('../../../utils/encryption');
+const crypto = require('crypto');
 
 module.exports = async (ctx, next) => {
-  console.log('\n\n\n\n\n------ web hook -----\n\n\n\n\n\n', ctx.request.body.read);
+  console.log('\n\n\n\n\n------ web hook -----\n\n\n\n\n\n', ctx.request.body.toString());
   fs.writeFileSync(
     path.resolve(__dirname, './webHookLogs.json'),
     `${JSON.stringify(ctx.request.body, null, 2)}\n\n
     ${JSON.stringify(ctx.request.header, null, 2)}\n\n
-    ${hmac({data: "web-hooks", secret: "qianyin"})}`,
+    ${crypto.createHmac('sha1', secret).update(ctx.request.body.toString()).digest('hex')}`,
     'utf-8'
   );
   console.log('---->>> ok \n\n\n\n\n\n\n');
   ctx.body = '成功';
 }
+
+// const secret = "your_secret_here";
+// const repo = "~/your_repo_path_here/";
+// ​
+// const http = require('http');
+// const crypto = require('crypto');
+// const exec = require('child_process').exec;
+// ​
+// http.createServer(function (req, res) {
+//     req.on('data', function(chunk) {
+//         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+// ​
+//         if (req.headers['x-hub-signature'] == sig) {
+//             exec('cd ' + repo + ' && git pull');
+//         }
+//     });
+// ​
+//     res.end();
+// }).listen(8080);
