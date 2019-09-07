c1ff6f77bb36ef73b79fe7972ef5ede429d56857
qianyin925 <qianyin95@qq.com>
2019-09-07 16:36:49 +0800

    refactor: webhooks 测试

--- app/service/webHooks/index.js
+++ app/service/webHooks/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const crypto = require('crypto');
 
 module.exports = async (ctx, next) => {
-  console.log('\n\n\n\n\n------ web hook -----\n\n\n\n\n\n', ctx.request.body.toString());
+  console.log('\n\n\n\n\n------ web hook ------\n\n\n\n\n\n', ctx.request.body.toString());
   fs.writeFileSync(
     path.resolve(__dirname, './webHookLogs.json'),
     `${JSON.stringify(ctx.request.body, null, 2)}\n\n
