7019315f6c64eaa86b8412c0ea19e1dfad03c32e
qianyin925 <qianyin95@qq.com>
2019-09-07 17:16:33 +0800

    refactor: webhooks 测试

--- app/service/webHooks/index.js
+++ app/service/webHooks/index.js
@@ -13,7 +13,7 @@ const sign = (data, secret) => {
 
 module.exports = async (ctx, next) => {
   const result = Buffer.from(ctx.request.header['x-hub-signature']).equals(Buffer.from(sign(ctx.request.body, "123456")))
-  console.log('\n\n\n\n\n------ web hook -------\n\n\n\n\n\n', result, '\n\n\n\n');
+  console.log('\n\n\n\n\n------ web hook --------\n\n\n\n\n\n', result, '\n\n\n\n');
 
   fs.writeFileSync(
     path.resolve(__dirname, './webHookLogs.json'),
