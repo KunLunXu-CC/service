97ae2418be109a41802d29ac1601994a5ee4f0f4
qianyin925 <qianyin95@qq.com>
2019-09-07 10:41:04 +0800

    refactor: webhooks 测试

--- app/route.js
+++ app/route.js
@@ -8,18 +8,7 @@ router.get('/', async (ctx, next) => {
   ctx.body = data;
 });
 
-
-router.post('/web-hooks', async (ctx, next) => {
-  console.log('\n\n\n\n\n------ web hook -----\n\n\n\n\n\n', ctx.request.body, typeof ctx.request.body);
-  fs.writeFileSync(
-    path.resolve(__dirname, './webHookLogs.json'),
-    `${JSON.stringify(ctx.request.body, null, 2)}\n\n
-    ${JSON.stringify(ctx.request.header, null, 2)}`,
-    'utf-8'
-  );
-  console.log('---->>> ok \n\n\n\n\n\n\n');
-  ctx.body = '成功';
-});
+router.post('/web-hooks', require('./service/webHooks'));
 
 module.exports = (app) => {
   app.use(router.routes()).use(router.allowedMethods());
---
+++ app/service/webHooks/index.js
@@ -0,0 +1,14 @@
+const { hash } = require('../../../utils/encryption');
+
+module.exports = async (ctx, next) => {
+  console.log('\n\n\n\n\n------ web hook -----\n\n\n\n\n\n', ctx.request.body, typeof ctx.request.body);
+  fs.writeFileSync(
+    path.resolve(__dirname, './webHookLogs.json'),
+    `${JSON.stringify(ctx.request.body, null, 2)}\n\n
+    ${JSON.stringify(ctx.request.header, null, 2)}\n\n
+    ${hash({data: "e4312b06110aad6ebc9a4ffab08b97187477574e", type: "sha1"})}`,
+    'utf-8'
+  );
+  console.log('---->>> ok \n\n\n\n\n\n\n');
+  ctx.body = '成功';
+}
