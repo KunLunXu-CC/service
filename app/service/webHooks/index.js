2e903084d6e5ad9e736720b86ed100ef119769e1
qianyin925 <qianyin95@qq.com>
2019-09-07 10:55:41 +0800

    refactor: webhooks

--- app/service/webHooks/index.js
+++ app/service/webHooks/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { hash } = require('../../../utils/encryption');
 
 module.exports = async (ctx, next) => {
-  console.log('\n\n\n\n\n------ web hook -------\n\n\n\n\n\n', ctx.request.body, typeof ctx.request.body);
+  console.log('\n\n\n\n\n------ web hook --------\n\n\n\n\n\n', ctx.request.body, typeof ctx.request.body);
   fs.writeFileSync(
     path.resolve(__dirname, './webHookLogs.json'),
     `${JSON.stringify(ctx.request.body, null, 2)}\n\n
