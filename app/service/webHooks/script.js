9272086831afa1e29b220b4fed8d0562931f1ed1
qianyin925 <qianyin95@gmail.com>
2019-09-08 09:41:56 +0800

    refactor: webhooks 测试

--- app/service/webHooks/script.js
+++ app/service/webHooks/script.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const shell = require('shelljs');
 
 module.exports.blog_service = async ({ body, header }) => {
-
-  console.log('===================>>>>>>>>> \n\n\n\n');
-  return '操作成功!';
+  const { repository, ref } = body; 
+  if (ref !== 'refs/heads/master'){return `${repository.name}push 非 master 分支`;}
+  if (shell.exec('git pull').code !== 0) {
+    return `${repository.name}代码拉取失败`;
+  }
+  return `${repository.name}操作成功!`;
 }
