ec473cd937b1e22700b6016def55283f5d8a315f
qianyin925 <qianyin95@gmail.com>
2019-09-08 15:02:20 +0800

    refactor: webhooks 测试

--- app/service/webHooks/index.js
+++ app/service/webHooks/index.js
@@ -13,12 +13,15 @@ const verify = ({ header, body }) => {
 }
 
 module.exports = async (ctx, next) => {
+  const { repository } = ctx.request.body;
   const result = verify(ctx.request);
   if (!result){
-    ctx.body = '身份验证失败';
-  } else {
+    ctx.body = `身份验证失败!`;
+  } else if(script[repository.name]){
     const { repository } = ctx.request.body;
     setTimeout(script[repository.name].bind(null, ctx.request), 0);
-    ctx.body = '成功';
+    ctx.body = '匹配成功, 将执行指定脚本';
+  } else {
+    ctx.body = '未定义该仓库的执行脚本!';
   }
 }
