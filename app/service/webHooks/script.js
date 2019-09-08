ab88dd3594e80dd67635d6b788c5b71a455c0c4d
qianyin925 <qianyin95@gmail.com>
2019-09-08 09:25:35 +0800

    refactor: webhookse 测试

--- app/service/webHooks/script.js
+++ app/service/webHooks/script.js
@@ -3,6 +3,6 @@ const path = require('path');
 
 module.exports.blog_service = async ({ body, header }) => {
 
-  console.log('================>>>>>>>>> \n\n\n\n');
+  console.log('===================>>>>>>>>> \n\n\n\n');
   return '操作成功!';
 }
