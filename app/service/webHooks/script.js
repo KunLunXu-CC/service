fb137489eb94ca24f2e4ed045c4520669b6e6c8e
qianyin925 <qianyin95@gmail.com>
2019-09-09 12:25:03 +0800

    refactor: 项目部署

--- app/service/webHooks/script.js
+++ app/service/webHooks/script.js
@@ -4,6 +4,8 @@ const shell = require('shelljs');
 
 module.exports.blog_service = async ({ body, header }) => {
   const { repository, ref } = body;
+  console.log('-------------\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+  shell.exec('ls -a ../../../../blog_client');
   if (ref !== 'refs/heads/master'){return `${repository.name} push 非 master 分支`;}
   if (shell.exec('git pull && npm i').code !== 0) {return `${repository.name}更新完成`;}
   return `${repository.name}操作成功!`;
