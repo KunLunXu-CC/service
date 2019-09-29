5d3740e57d28ab461f71224870a584d74c7873d3
qianyin925 <qianyin95@gmail.com>
2019-09-29 11:05:52 +0800

    refactor: 文件上传

--- app/route.js
+++ app/route.js
@@ -5,7 +5,10 @@ router.get('/', async (ctx, next) => {
   const data = '这是一首页';
   ctx.body = data;
 });
+
 router.post('/web-hooks', require('./service/webHooks'));
+router.post('/photo/upload', require('./service/photo/upload'));
+
 module.exports = (app) => {
   app.use(router.routes()).use(router.allowedMethods());
 }
---
+++ app/service/photo/upload.js
@@ -0,0 +1,4 @@
+
+module.exports = async (ctx, next) => {
+  ctx.body = "上传完成";
+}
