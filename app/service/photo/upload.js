8eda8f9a8d3ba2b7d4e2bdca411151e7e79e1ea9
qianyin925 <qianyin95@gmail.com>
2019-09-30 17:02:42 +0800

    refactor: 文件上传 bug 修复

--- app/service/photo/upload.js
+++ app/service/photo/upload.js
@@ -2,8 +2,19 @@
 const _ = require('lodash');
 const qiniu = require('../../../utils/qiniu');
 
-module.exports = async (ctx, next) => {
+/**
+ * 获取上传文件列表
+ * @param {Object} ctx 上下文 
+ * @return {Object[]}
+ */
+const getFiles = (ctx) => {
+  // 需要注意的是如果只上传了一个文件那么 files 实际上就是一个 File 对象
   const files = _.get(ctx, 'request.files.file', []);
+  return _.isArray(files) ? files : [ files ];
+}
+
+module.exports = async (ctx, next) => {
+  const files = getFiles(ctx);
 
   for (let i = 0; i < files.length; i++) {
     const file = files[i];
