ebe2649ab25e25f386ef967c67b3941f5cbc8682
qianyin925 <qianyin95@gmail.com>
2019-09-30 16:04:50 +0800

    refactor: 接入邮件功能

--- config/system/development.js
+++ config/system/development.js
@@ -2,17 +2,21 @@ const fs = require('fs');
 const path = require('path');
 module.exports = {
   port: 4000,                       // 系统应用端口
+
   graphql: {                        // graphql 配置
     path: '/graphql',               // graphql 应用路由
   },
+
   mongo: {                          // mongo 配置
     debug: true,                    // 是否启用 debug
     port: 27017,                    // 端口号
     host: '127.0.0.1',              // 主机
     database: 'blog',               // 数据库名
   },
+
   defaultUser: 'tourist',           // 默认用户(游客)账号
-  webHookSecret: '123456',          // web hooks secret
+  webHookSecret: '******',          // web hooks secret
+
   // 登录私钥
   privateKey: fs.readFileSync(path.resolve(__dirname, './private.dev.key')),
   // 登录公钥
@@ -22,8 +26,19 @@ module.exports = {
   qiniu: {
     bucket: 'qianyin-blog',                                   // 存储空间
     zone: 'Zone_z2',                                          // 存储区域(华南)
-    accessKey: '4ujsdasdfH4VrUylwdEm12XjpJbAgdp7ovVdEa5p',    // AK
-    secretKey: 'APdV112asddA3Jb3_KymCf4csPekcOmYuw4BMF3_',    // SK
+    accessKey: '********************12XjpJbAgdp7ovVdEa5p',    // AK
+    secretKey: '********************Cf4csPekcOmYuw4BMF3_',    // SK
     cdn: 'http://qiniu.cdn.blog.qianyin925.com',              // cdn
+  },
+
+  // SMTP 邮件服务配置
+  smtp: {
+    host: "smtp.***.com",           // 邮件服务地址
+    secure: true,                   // 是否使用 TLS / STARTTLS的安全电子邮件传递, 将使用 465 端口
+    auth: {
+      user: '***********@***.com',  // 用户
+      pass: '****************',     // 授权密码
+    },
+    notice: '*********@qq.com',     // 收件人(站内消息通知)
   },
 };
--- utils/emailer.js
+++ utils/emailer.js
@@ -1,73 +1,32 @@
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+const { smtp } = require('../config/system');
 
-// async function main() {
-//     let transporter = nodemailer.createTransport({
-//         host: 'smtp.ethereal.email',
-//         port: 587,
-//         secure: false, // true for 465, false for other ports
-//         auth: {
-//             user: testAccount.user, // generated ethereal user
-//             pass: testAccount.pass // generated ethereal password
-//         }
-//     });
-
-//     // send mail with defined transport object
-//     let info = await transporter.sendMail({
-//         from: '"Fred Foo 👻" <foo@example.com>', // sender address
-//         to: 'bar@example.com, baz@example.com', // list of receivers
-//         subject: 'Hello ✔', // Subject line
-//         text: 'Hello world?', // plain text body
-//         html: '<b>Hello world?</b>' // html body
-//     });
-
-//     console.log('Message sent: %s', info.messageId);
-//     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-//     // Preview only available when sending through an Ethereal account
-//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-//     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-// }
-
-// main().catch(console.error);
-
-// //引入模块
-// const nodemailer = require('nodemailer');
-
-// //设置邮箱配置
-// let transporter = nodemailer.createTranspoter({
-//   host:'',//邮箱服务的主机，如smtp.qq.com
-//   port:'',//对应的端口号
-//   //开启安全连接
-//   secure:false,
-//   //secureConnection:false,
-//   //用户信息
-//   auth:{
-//     user:'',
-//     pass:''
-//   }
-// });
-
-// //设置收件人信息
-// let mailOptions={
-//   from:'',//谁发的
-//   to:'',//发给谁
-//   subject:'',//主题是什么
-//   text:'',//文本内容
-//   html:'',//html模板
-
-//    //附件信息
-//   attachments:[
-//   {
-//       filename:'',
-//       path:'',
-//     }
-//   ]
-// };
-
-// //发送邮件
-// transporter.sendMail(mailOptions,(error,info)=>{
-//   if(error)
-//     return console.log(error);
-//    console.log(`Message: ${info.messageId}`);
-//    console.log(`sent: ${info.response}`);
-// });
+/**
+ * 邮箱发送
+ * @param {String} message.to            收件人列表, 多个收件人用 , 分割
+ * @param {String} message.subject       邮件主题
+ * @param {String} message.html          邮件内容(html)
+ * @param {String} message.text          邮件内容(文本), 如果设置了  html 则不显示该内容
+ * @param {Object[]} message.attachments 附件列表 [{filename: '附件名.md', path: '文件本地路径'}...] 更多配置参考： https://nodemailer.com/message/attachments/
+ * @example
+ * await emailer({
+ *   // to: 'xxx.@qq.com', 
+ *   subject: '邮箱主题', 
+ *   html: '<div>邮箱 html</div>',
+ *   text: '邮箱内容',
+ *   attachments: [{ filename: 'emailer.js', path: path.resolve(__dirname, './emailer.js') }]
+ * }).catch(console.error);
+ */
+module.exports = async (message) => {
+  // 1. 获取配置信息
+  const { notice, ...options } = smtp;
+  // 2. 如果发送信息为设置收件人则默认发送到指定邮箱(站内消息通知邮箱)
+  !message.to && (message.to = notice);
+  // 3. 创建连接池
+  let transporter = nodemailer.createTransport(options);
+  // 4. 发送邮箱
+  return await transporter.sendMail({
+    ...message,
+    from: options.auth.user,
+  });
+}
