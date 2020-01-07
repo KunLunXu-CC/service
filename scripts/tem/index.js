dfc1e51897e8b7687692becf2ed8cfbb1c9e9c44
qianyin925 <qianyin95@gmail.com>
2020-01-07 16:43:29 +0800

    build: 更新数据脚本

--- scripts/tem/index.js
+++ scripts/tem/index.js
@@ -6,17 +6,17 @@ const script = async () => {
   const { id, auth } = await mongoDB.Role.findOne({ name : 'admin' });
   if (!id){return false;}
   console.log('-------->>> 更新 admin 权限');
+  const _auth = auth.map(v => {
+    if (v.code === 'lift'){
+      v.code = 'diary'
+    }
+    return v;
+  });
+
   await mongoDB.Role.updateMany({ _id: id }, {
-    auth: [ ... auth, {
-      code: 'lift', readable: BOOLEAN.TRUE, writable:BOOLEAN.TRUE
-    }]
+    auth: _auth
   }, {});
   console.log('--->>> 更新 admin 权限成功');
-
-  console.log('----->>> 修改 ES6 icon');
-  await mongoDB.Tag.updateMany({ name: "ES6" }, { icon: "icon-js" }, {});
-  console.log('----->>> 修改 ES6 icon 成功');
-
 };
 
 script();
