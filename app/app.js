35a73e673f00802dc07caf576e0b31995426045a
qianyin925 <qianyin95@gmail.com>
2020-01-10 19:22:50 +0800

    refactor: 【server】开启 HTTPS

--- app/app.js
+++ app/app.js
@@ -3,7 +3,9 @@ const middleware = require('./middleware');
 const config = require('../config/system');
 const graphql = require('./graphql');
 const router = require('./route');
+const https = require('https');
 const cron = require('./cron');
+const path = require('path');
 const Koa = require('koa');
 const db = require('./db');
 const app = new Koa();
@@ -15,3 +17,10 @@ graphql(app);
 cron(app);
 
 app.listen(config.port, printStartCharPattern);
+
+const options = {
+  key: fs.readFileSync(path.resolve(__dirname, '../docker/nginx/ssl.key')),
+  cert: fs.readFileSync(path.resolve(__dirname, '../docker/nginx/ssl.crt')),
+};
+
+https.createServer(options, app).listen(config.port, printStartCharPattern);
