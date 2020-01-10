d2ed244e365e5e385f2872ddd5669febbf6947f6
qianyin925 <qianyin95@gmail.com>
2020-01-10 09:29:47 +0800

    build: 文档、脚本修改

--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
-const { createProKey } = require('./rsa');
+const { createSecretKey } = require('./rsa');
 
 module.exports = {
-  createProKey,                                   // config/system 下创建秘钥
+  createSecretKey,                                // config/system 下创建秘钥
   initDB: require('./initDB'),                    // 执行初始化脚本
   restartDocker: require('./restartDocker'),      // 重启 docker(将更新docker容器)
   default: async () => {},
