cb4b7b45fad47fc9973dd5251ba9578b75596ea2
qianyin925 <qianyin95@gmail.com>
2020-01-11 12:23:34 +0800

    refactor: 【server】webhook 修改

--- app/service/webHooks/blogService.js
+++ app/service/webHooks/blogService.js
@@ -6,43 +6,43 @@ const shell = require('shelljs');
 module.exports = async ({ body, header }) => {
   const { repository, ref } = body;
   if (ref !== 'refs/heads/master'){
-    console.log('[webhooks] 提交非 master 分支代码'.red);
+    console.log('[webhooks] 提交非 master 分支代码');
     return false;
   }
 
   // 1. 提示: 脚本开始
-  console.log(`=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`.green);
+  console.log(`=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`);
 
   // 2. 撤销 git 的所有本地修改
   if (shell.exec(`
     git fetch --all  && \
     git reset --hard origin/master  && \
-    git clean -df && \
+    git clean -df
   `).code !== 0) {
-    console.log('1. [fail] 撤销 git 的所有本地修改失败'.red);
+    console.log('1. [fail] 撤销 git 的所有本地修改失败');
     return false;
   }
-  console.log('1. [success] 撤销 git 的所有本地修改成功'.green);
+  console.log('1. [success] 撤销 git 的所有本地修改成功');
 
   // 3. 拉取代码
   if (shell.exec('git pull').code !== 0) {
-    console.log('2. [fail] 拉取代码失败'.red);
+    console.log('2. [fail] 拉取代码失败');
     return false;
   }
-  console.log('2. [success] 拉取代码成功'.green);
+  console.log('2. [success] 拉取代码成功');
 
 
   // 4. 安装依赖
   if (shell.exec('npm i').code !== 0) {
-    console.log('3. [fail] 安装依赖失败'.red);
+    console.log('3. [fail] 安装依赖失败');
     return false;
   }
-  console.log('3. [success] 安装依赖成功'.green);
+  console.log('3. [success] 安装依赖成功');
 
   // 5. 设置权限
   shell.chmod(755, '-R', '.');
-  console.log('4. [success] 设置权限成功'.green);
+  console.log('4. [success] 设置权限成功');
 
   // 6. 提示：完成
-  console.log(`=======>>>> [webhooks] ${repository.name}: success <<<<=======`.green);
+  console.log(`=======>>>> [webhooks] ${repository.name}: success <<<<=======`);
 }
