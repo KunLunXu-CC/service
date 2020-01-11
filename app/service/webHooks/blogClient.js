7d51566c945baed6e3253ac9b8a47aba89de1309
qianyin925 <qianyin95@gmail.com>
2020-01-11 12:19:21 +0800

    refactor: 【server】webhook 修改

--- app/service/webHooks/blogClient.js
+++ app/service/webHooks/blogClient.js
@@ -5,49 +5,54 @@ const shell = require('shelljs');
 
 module.exports = async ({ body, header }) => {
   const { repository, ref } = body;
-  // 1. 提示: 脚本开始
-  console.log(`=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`.green);
-
-  // 2. 条件判断： 只有 master 分支代码的提交才做更新
   if (ref !== 'refs/heads/master'){
-    console.log('[fail][2] 提交非 master 分支代码');
+    console.log('[webhooks] 提交非 master 分支代码'.red);
     return false;
   }
 
-  // 3. 进入项目目录
-  shell.cd('html');
+  // 1. 提示: 脚本开始
+  console.log(`=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`.green);
 
-  // 4. 撤销 git 的所有本地修改
+  // 2. 进入项目目录
+  shell.cd('html');
+  console.log('1. [success] 进入项目目录'.green);
+
+  // 3. 撤销 git 的所有本地修改
   if (shell.exec(`
     git fetch --all  && \
     git reset --hard origin/master  && \
     git clean -df && \
   `).code !== 0) {
-    console.log('[fail][4] 撤销 git 的所有本地修改失败');
+    console.log('2. [fail] 撤销 git 的所有本地修改失败');
     return false;
   }
+  console.log('2. [success] 撤销 git 的所有本地修改成功'.green);
 
-  // 5. 拉取代码
+  // 4. 拉取代码
   if (shell.exec('git pull').code !== 0) {
-    console.log('[fail][5] 拉取代码失败');
+    console.log('3. [fail] 拉取代码失败');
     return false;
   }
+  console.log('3. [success] 拉取代码成功'.green);
 
-  // 6. 安装依赖
+  // 5. 安装依赖
   if (shell.exec('npm i').code !== 0) {
-    console.log('[fail][6] 安装依赖失败');
+    console.log('4. [fail] 安装依赖失败');
     return false;
   }
+  console.log('4. [success] 安装依赖成功'.green);
 
   // 6. 打包编译
   if (shell.exec('npm run build').code !== 0) {
-    console.log('[fail][4] 打包编译失败');
+    console.log('6. [fail] 打包编译失败');
     return false;
   }
+  console.log('6. [success] 打包编译成功'.green);
 
-  // 6. 设置权限
+  // 7. 设置权限
   shell.chmod(755, '-R', '.');
+  console.log('7. [success] 设置权限成功'.green);
 
-  // 7. 提示：完成
+  // 8. 提示：完成
   console.log(`=======>>>> [webhooks] ${repository.name}: success <<<<=======`.green);
 }
--- app/service/webHooks/blogService.js
+++ app/service/webHooks/blogService.js
@@ -5,40 +5,44 @@ const shell = require('shelljs');
 
 module.exports = async ({ body, header }) => {
   const { repository, ref } = body;
-  // 1. 提示: 脚本开始
-  console.log(`=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`.green);
-
-  // 2. 条件判断： 只有 master 分支代码的提交才做更新
   if (ref !== 'refs/heads/master'){
-    console.log('[fail][2] 提交非 master 分支代码'.red);
+    console.log('[webhooks] 提交非 master 分支代码'.red);
     return false;
   }
 
-  // 3. 撤销 git 的所有本地修改
+  // 1. 提示: 脚本开始
+  console.log(`=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`.green);
+
+  // 2. 撤销 git 的所有本地修改
   if (shell.exec(`
     git fetch --all  && \
     git reset --hard origin/master  && \
     git clean -df && \
   `).code !== 0) {
-    console.log('[fail][3] 撤销 git 的所有本地修改失败'.red);
+    console.log('1. [fail] 撤销 git 的所有本地修改失败'.red);
     return false;
   }
+  console.log('1. [success] 撤销 git 的所有本地修改成功'.green);
 
-  // 4. 拉取代码
+  // 3. 拉取代码
   if (shell.exec('git pull').code !== 0) {
-    console.log('[fail][4] 拉取代码失败'.red);
+    console.log('2. [fail] 拉取代码失败'.red);
     return false;
   }
+  console.log('2. [success] 拉取代码成功'.green);
 
-  // 5. 安装依赖
+
+  // 4. 安装依赖
   if (shell.exec('npm i').code !== 0) {
-    console.log('[fail][5] 安装依赖失败'.red);
+    console.log('3. [fail] 安装依赖失败'.red);
     return false;
   }
+  console.log('3. [success] 安装依赖成功'.green);
 
-  // 6. 设置权限
+  // 5. 设置权限
   shell.chmod(755, '-R', '.');
+  console.log('4. [success] 设置权限成功'.green);
 
-  // 7. 提示：完成
+  // 6. 提示：完成
   console.log(`=======>>>> [webhooks] ${repository.name}: success <<<<=======`.green);
 }
