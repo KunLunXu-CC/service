918fcc73601dc98e3408f6c632b97493e5f85daa
qianyin925 <qianyin95@gmail.com>
2020-01-11 10:46:59 +0800

    feat: 【server】pm2 修改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps : [{
     name: 'app',
-    script: './app/app.js',
-
     watch: true,
-    ignore_watch: ['node_modules', 'docker', 'html', '.git'],
+    script: './app/app.js',
+    watch: ['app', 'config', 'models', 'utils'],
+    // ignore_watch: ['node_modules', 'docker', 'html', '.git'],
     watch_delay: 1000 * 60 * 5,
   }]
 }
