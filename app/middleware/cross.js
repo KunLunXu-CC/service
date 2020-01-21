3e75ae3c4e7ff188a9848f0664e179308d3339b2
qianyin925 <qianyin95@gmail.com>
2020-01-21 09:42:09 +0800

    feat: 【server】跨域配置中支持正则

--- app/middleware/cross.js
+++ app/middleware/cross.js
@@ -1,15 +1,16 @@
 /**
  * 跨域设置
  */
+const _ = require('lodash');
 const cors = require('koa2-cors');
 const { corsOrigin } = require('../../config/system');
 
 module.exports = cors({
   origin: ctx => {
-    if (ctx.url === '/test') {
+    if (_.isRegExp(corsOrigin) && corsOrigin.test(ctx.url)) {
       // 这里可以对请求 url 进行匹配, 进行更复杂的设置,
       // 比如允许多个域名、多个端口进行跨域请求、或者允许指定模式的 url 进行跨域请求
-      return false;
+      return '*';
     }
     return corsOrigin;
   },
