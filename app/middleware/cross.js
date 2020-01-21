360a149116e66162f7ecb56185de5f194df3f70f
qianyin925 <qianyin95@gmail.com>
2020-01-21 10:37:46 +0800

    refactor: 【server】跨域配置中支持正则

--- app/middleware/cross.js
+++ app/middleware/cross.js
@@ -7,10 +7,10 @@ const { corsOrigin } = require('../../config/system');
 
 module.exports = cors({
   origin: ctx => {
-    if (_.isRegExp(corsOrigin) && corsOrigin.test(ctx.url)) {
+    if (_.isRegExp(corsOrigin) && corsOrigin.test(ctx.origin)) {
       // 这里可以对请求 url 进行匹配, 进行更复杂的设置,
       // 比如允许多个域名、多个端口进行跨域请求、或者允许指定模式的 url 进行跨域请求
-      return '*';
+      return ctx.origin;
     }
     return corsOrigin;
   },
