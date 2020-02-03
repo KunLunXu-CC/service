a57139df9eb90fbcc8ea63deca733ab4c4dec0a1
qianyin925 <qianyin95@gmail.com>
2020-02-03 18:15:02 +0800

    feat: 【server】nginx 重定向配置

--- app/route.js
+++ app/route.js
@@ -5,6 +5,9 @@ const router = new Router();
 // 路由配置
 router.get('/', async (ctx, next) => { ctx.body = '这是一首页'; });
 
+// 微信开发
+router.get('/wechat', require('./wechat'));
+
 router.post('/web-hooks', require('./service/webHooks'));
 router.post('/photo/upload', require('./service/photo/upload'));
 
---
+++ app/wechat/index.js
@@ -0,0 +1,1 @@
+module.exports = async (ctx, next) => { ctx.body = '这是一首页'; }
