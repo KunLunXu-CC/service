260c2ae949de6fff3642f3e537429a5d8b9cdb46
qianyin925 <qianyin95@gmail.com>
2020-02-04 15:02:46 +0800

    feat: 【server】微信公众号服务器配置

--- app/wechat/index.js
+++ app/wechat/index.js
@@ -1,1 +1,20 @@
-module.exports = async (ctx, next) => { ctx.body = '这是一首页1'; }
+var sha1 = require("sha1");
+
+var config = {
+  "appID": "************",
+  "appsecret": "*************************",
+  "token": "*************************",
+  "EncodingAESKey": "*************************",
+}
+
+module.exports = async (ctx, next) => {
+  await next();
+
+  // 获取微信服务器发送的数据
+  const { signature, timestamp, nonce, echostr } = ctx.query;
+
+  // token、timestamp、nonce三个参数进行字典序排序
+  const arr = [config.token, timestamp, nonce].sort().join('');
+
+  ctx.body = sha1(arr) === signature ? echostr : 'mismatch';
+}
