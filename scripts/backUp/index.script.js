5887399ab08be52313e6e948b21842b632ce12ed
qianyin925 <qianyin95@gmail.com>
2020-03-18 13:28:34 +0800

    refactor: 【server】脚本系统架构调整

--- scripts/backUp/index.script.js
+++ scripts/backUp/index.script.js
@@ -1,9 +1,17 @@
 const inquirer = require('inquirer');
 
+// 选项
+const choices = [
+  {
+    name: '备份数据库',
+    exec: require('./databases'),
+  },
+];
+
 module.exports = {
   name: '备份数据',
   exec: async () => {
-    const { dest } = await inquirer.prompt([
+    const { dest, execNames } = await inquirer.prompt([
       {
         name: 'dest',
         type: 'input',
@@ -11,16 +19,17 @@ module.exports = {
         message: '备份文件存储目录',
       },
       {
-        type: 'list',
-        name: 'script',
-        choices: [
-          {
-            name: '',
-          },
-        ],
+        choices,
+        type: 'checkbox',
+        name: 'execNames',
         message: '选择要备份的内容',
-      }
+      },
     ]);
-    console.log('拷贝文件路径', dest);
+
+    // 执行所有脚本
+    execNames.forEach( async name => {
+      const exec = choices.find(v => v.name === name).exec;
+      await exec({ dest });
+    });
   },
 };
--- scripts/index.js
+++ scripts/index.js
@@ -5,33 +5,50 @@ const inquirer = require('inquirer');
 
 const { requireFiles } = require('../utils');
 
-// 加载所有脚本配置:
-// [{ name: '显示名称(不能重复)', exec: async () => {待执行脚本}}]
-const scripts = [];
-requireFiles({
-  dir: path.resolve(__dirname, '.'),
-  filter: file => (path.basename(file) !== 'index.script.js'),
-  handler: dest => {
-    scripts.push(require(dest));
-  }
-});
+// 1. 获取 choices 配置
+const getChoices = (choices = []) => {
+  // 1. 加载所有脚本配置:
+  // [{ name: '显示名称(不能重复)', exec: async () => {待执行脚本}}]
+  requireFiles({
+    dir: path.resolve(__dirname, '.'),
+    filter: file => (path.basename(file) !== 'index.script.js'),
+    handler: dest => {
+      choices.push(require(dest));
+    }
+  });
+  return choices;
+}
 
-// 调用 inquirer, 选择需要执行的脚本
-inquirer.prompt([{
-  type: 'list',
-  name: 'script',
-  choices: scripts,
-  message: '选择脚本',
-}]).then(async ({ script }) => {
-  const exec = (scripts.find(v => v.name  === script) || {}).exec;
-  console.log(chalk.yellow(`【${script}】开始执行脚本\n`));
-  if (!exec) {
-    console.log(chalk.red(`【${script}】未找到脚本!\n`));
+// 2. 主方法
+const main = async () => {
+  const choices = getChoices();
+
+  // 调用 inquirer, 选择需要执行的脚本
+  const { scriptNames } = await inquirer.prompt([{
+    choices,
+    type: 'checkbox',
+    message: '选择脚本',
+    name: 'scriptNames',
+  }]);
+
+  if (scriptNames.length === 0) {
+
   } else {
-    await exec();
-    console.log(chalk.green(`【${script}】脚本执行完成!\n`));
+    ora().warn('warn!');
+    ora().succeed('succeed!');
+    ora().fail('fail!');
+    ora().warn('warn!');
+    ora().info('info!');
+    scriptNames.forEach(async name => {
+      try {
+        await choices.find(v => v.name  === name).exec();
+      } catch(err) {
+        console.log(chalk.red('脚本执行错误!\n'));
+        console.log(err);
+      }
+    });
   }
-}).catch ( err => {
-  console.log(chalk.red('脚本执行错误!\n'));
-  console.log(err);
-});
+}
+
+// 执行主方法
+main();
