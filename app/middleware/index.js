f8f8e2586f1524e0508cb098b376c1e09a51ffe0
qianyin925 <qianyin95@gmail.com>
2020-03-21 15:15:56 +0800

    refactor: 【server】项目部署流程整理

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -1,9 +1,6 @@
 /** 放一些比较杂的中间件 */
-const staticServe = require('koa-static');
-const koaBody = require('koa-body');
-const path = require('path');
-
 const setUser = require('./setUser');
+const koaBody = require('koa-body');
 const logger = require('./logger');
 const cross = require('./cross');
 
@@ -19,7 +16,4 @@ module.exports = (app) => {
 
   // koa body 解析, 支持文件上传解析
   app.use(koaBody({ multipart: true }));
-
-  // 静态服务（优先于 route 执行， 注意避免路由冲突）
-  app.use(staticServe(path.resolve(__dirname, '../static')));
 }
