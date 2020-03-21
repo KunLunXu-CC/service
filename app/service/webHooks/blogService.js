c14050d648f8ea66f4cb3686fcd52ebd6b63d918
qianyin925 <qianyin95@gmail.com>
2020-03-21 16:33:41 +0800

    refactor: 【server】项目部署流程整理

--- app/service/webHooks/blogService.js
+++ app/service/webHooks/blogService.js
@@ -2,59 +2,59 @@ const path = require('path');
 const shell = require('shelljs');
 
 module.exports = async ({ body, header }) => {
-  const { repository, ref } = body;
-  if (ref !== 'refs/heads/master'){
-    console.log('[webhooks] 提交非 master 分支代码');
-    return false;
-  }
+  // const { repository, ref } = body;
+  // if (ref !== 'refs/heads/master'){
+  //   console.log('[webhooks] 提交非 master 分支代码');
+  //   return false;
+  // }
 
-  // 1. 提示: 脚本开始
-  console.log(`=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`);
+  // // 1. 提示: 脚本开始
+  // console.log(`=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`);
 
-  // 2. 进入项目目录
-  shell.cd(path.resolve(__dirname, '../../../'));
-  console.log('1. [success] 进入项目目录', shell.pwd());
+  // // 2. 进入项目目录
+  // shell.cd(path.resolve(__dirname, '../../../'));
+  // console.log('1. [success] 进入项目目录', shell.pwd());
 
-  // 3. 撤销 git 的所有本地修改
-  if (shell.exec(`
-    git fetch --all  && \
-    git reset --hard origin/master  && \
-    git clean -df
-  `).code !== 0) {
-    console.log('2. [fail] 撤销 git 的所有本地修改失败');
-    return false;
-  }
-  console.log('2. [success] 撤销 git 的所有本地修改成功');
+  // // 3. 撤销 git 的所有本地修改
+  // if (shell.exec(`
+  //   git fetch --all  && \
+  //   git reset --hard origin/master  && \
+  //   git clean -df
+  // `).code !== 0) {
+  //   console.log('2. [fail] 撤销 git 的所有本地修改失败');
+  //   return false;
+  // }
+  // console.log('2. [success] 撤销 git 的所有本地修改成功');
 
-  // 4. 拉取代码
-  if (shell.exec('git pull').code !== 0) {
-    console.log('3. [fail] 拉取代码失败');
-    return false;
-  }
-  console.log('3. [success] 拉取代码成功');
+  // // 4. 拉取代码
+  // if (shell.exec('git pull').code !== 0) {
+  //   console.log('3. [fail] 拉取代码失败');
+  //   return false;
+  // }
+  // console.log('3. [success] 拉取代码成功');
 
-  // 5. 安装依赖
-  if (
-    shell.exec('npm i').code !== 0 &&
-    shell.exec('npm i --only=dev').code !== 0
-  ) {
-    console.log('4. [fail] 安装依赖失败');
-    return false;
-  }
-  console.log('4. [success] 安装依赖成功');
+  // // 5. 安装依赖
+  // if (
+  //   shell.exec('npm i').code !== 0 &&
+  //   shell.exec('npm i --only=dev').code !== 0
+  // ) {
+  //   console.log('4. [fail] 安装依赖失败');
+  //   return false;
+  // }
+  // console.log('4. [success] 安装依赖成功');
 
-  // 6. 设置权限
-  shell.chmod('-R', 777, '.');
-  console.log('5. [success] 设置权限成功');
+  // // 6. 设置权限
+  // shell.chmod('-R', 777, '.');
+  // console.log('5. [success] 设置权限成功');
 
-  // 7. 提示：完成
-  console.log(`=======>>>> [webhooks] ${repository.name}: success <<<<=======`);
+  // // 7. 提示：完成
+  // console.log(`=======>>>> [webhooks] ${repository.name}: success <<<<=======`);
 
-  console.log('6. [success] 接下来将重启应用');
-  console.log(`当前位置: ${shell.pwd()}`);
-  // 8. 重启
-  if (shell.exec('npm run restart:pro').code !== 0) {
-    console.log('6. [fail] 重启失败');
-    return false;
-  }
+  // console.log('6. [success] 接下来将重启应用');
+  // console.log(`当前位置: ${shell.pwd()}`);
+  // // 8. 重启
+  // if (shell.exec('npm run restart:pro').code !== 0) {
+  //   console.log('6. [fail] 重启失败');
+  //   return false;
+  // }
 }
