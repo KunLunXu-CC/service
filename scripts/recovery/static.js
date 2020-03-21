0c4e43b3da68a929d403cf70fd03909416e5a9ab
qianyin925 <qianyin95@gmail.com>
2020-03-21 17:06:15 +0800

    refactor: 【server】项目部署流程整理

--- scripts/recovery/static.js
+++ scripts/recovery/static.js
@@ -1,14 +1,13 @@
 const path = require('path');
 const shell = require('shelljs');
+const { mkdirPath } = require('../../utils');
 
 //　恢复静态目录 app/static
 module.exports = {
   name: '恢复静态目录 app/static',
   exec: async ({ dest }) => {
-    console.log(`开始恢复静态目录！`);
-    shell.exec(`sudo rm -rf ${path.resolve(__dirname, '../../app/static')}`);
+    mkdirPath(path.resolve(__dirname, '../../app/static'));
     if (shell.exec(`
-      sudo mkdir ${path.resolve(__dirname, '../../app/static')}
       sudo cp -rf ${dest}/static/* ${path.resolve(__dirname, '../../app/static')}
     `).code === 0) {
       console.log('静态目录已恢复');
