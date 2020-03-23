a019655b8ed9117dd681f58e4973a6498e2984ce
qianyin925 <qianyin95@gmail.com>
2020-03-23 10:29:23 +0800

    refactor: 【server】webhook 服务重新测试编写

--- app/service/webHooks/blogClient.js
+++ app/service/webHooks/blogClient.js
@@ -9,11 +9,11 @@ module.exports = async ({ body, header }) => {
   }
 
   // 1. 提示: 脚本开始
-  console.log(`=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`);
+  console.log(`\n\n\n\n=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`);
 
   // 2. 进入项目目录
   shell.cd(path.resolve(__dirname, '../../../docker/nginx/html'));
-  console.log('1. [success] 进入项目目录', shell.pwd());
+  console.log('\n1. [success] 进入项目目录', shell.pwd());
 
   // 3. 撤销 git 的所有本地修改
   if (shell.exec(`
@@ -21,39 +21,39 @@ module.exports = async ({ body, header }) => {
     git reset --hard origin/master  && \
     git clean -df
   `).code !== 0) {
-    console.log('2. [fail] 撤销 git 的所有本地修改失败');
+    console.log('\n2. [fail] 撤销 git 的所有本地修改失败');
     return false;
   }
-  console.log('2. [success] 撤销 git 的所有本地修改成功');
+  console.log('\n2. [success] 撤销 git 的所有本地修改成功');
 
   // 4. 拉取代码
   if (shell.exec('git pull').code !== 0) {
-    console.log('3. [fail] 拉取代码失败');
+    console.log('\n3. [fail] 拉取代码失败');
     return false;
   }
-  console.log('3. [success] 拉取代码成功');
+  console.log('\n3. [success] 拉取代码成功');
 
   // 5. 安装依赖
   if (
     shell.exec('npm i').code !== 0 &&
     shell.exec('npm i --only=dev').code !== 0
   ) {
-    console.log('4. [fail] 安装依赖失败');
+    console.log('\n4. [fail] 安装依赖失败');
     return false;
   }
-  console.log('4. [success] 安装依赖成功');
+  console.log('\n4. [success] 安装依赖成功');
 
   // 6. 打包编译
   if (shell.exec('npm run build:pro').code !== 0) {
-    console.log('5. [fail] 打包编译失败');
+    console.log('\n5. [fail] 打包编译失败');
     return false;
   }
-  console.log('5. [success] 打包编译成功');
+  console.log('\n5. [success] 打包编译成功');
 
   // 7. 设置权限
   shell.chmod('-R', 777, '.');
-  console.log('6. [success] 设置权限成功');
+  console.log('\n6. [success] 设置权限成功');
 
   // 8. 提示：完成
-  console.log(`=======>>>> [webhooks] ${repository.name}: success <<<<=======`);
+  console.log(`\n=======>>>> [webhooks] ${repository.name}: success <<<<=======\n\n\n\n`);
 }
