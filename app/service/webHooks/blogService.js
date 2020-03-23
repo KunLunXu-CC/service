c5d57eca98fa590dc9c9eb7f5e0245a348d5c8e7
qianyin925 <qianyin95@gmail.com>
2020-03-23 10:39:41 +0800

    refactor: 【server】webhook 服务重新测试编写

--- app/service/webHooks/blogService.js
+++ app/service/webHooks/blogService.js
@@ -9,11 +9,13 @@ module.exports = async ({ body, header }) => {
   }
 
   // 1. 提示: 脚本开始
-  console.log(`\n\n\n\n=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`);
+  new Array(10).fill(1).forEach(v => (console.log(`
+    =======>>>> [webhooks] ${repository.name}: submit new code <<<<=======
+  `)));
 
   // 2. 进入项目目录
   shell.cd(path.resolve(__dirname, '../../../'));
-  console.log(`\n1. [success] 进入项目目录: ${shell.pwd()}`);
+  console.log(`1. [success] 进入项目目录: ${shell.pwd()}`);
 
   // 3. 撤销 git 的所有本地修改
   if (shell.exec(`
@@ -21,40 +23,42 @@ module.exports = async ({ body, header }) => {
     git reset --hard origin/master  && \
     git clean -df
   `).code !== 0) {
-    console.log('\n2. [fail] 撤销 git 的所有本地修改失败');
+    console.log('2. [fail] 撤销 git 的所有本地修改失败');
     return false;
   }
-  console.log('\n2. [success] 撤销 git 的所有本地修改成功');
+  console.log('2. [success] 撤销 git 的所有本地修改成功');
 
   // 4. 拉取代码
   if (shell.exec('git pull').code !== 0) {
-    console.log('\n3. [fail] 拉取代码失败');
+    console.log('3. [fail] 拉取代码失败');
     return false;
   }
-  console.log('\n3. [success] 拉取代码成功');
+  console.log('3. [success] 拉取代码成功');
 
   // 5. 安装依赖: npm i 安装生产环境下依赖、--only=dev 则是安装开发环境下依赖
   if (
     shell.exec('npm i').code !== 0 &&
     shell.exec('npm i --only=dev').code !== 0
   ) {
-    console.log('\n4. [fail] 安装依赖失败');
+    console.log('4. [fail] 安装依赖失败');
     return false;
   }
-  console.log('\n4. [success] 安装依赖成功');
+  console.log('4. [success] 安装依赖成功');
 
   // 6. 设置权限
   shell.chmod('-R', 777, '.');
-  console.log('\n5. [success] 设置权限成功');
+  console.log('5. [success] 设置权限成功');
 
   // 7. 提示：完成
-  console.log(`=======>>>> [webhooks] ${repository.name}: success <<<<=======\n\n\n\n`);
+  new Array(10).fill(1).forEach(v => (console.log(`
+    =======>>>> [webhooks] ${repository.name}: success <<<<=======
+  `)));
 
-  console.log('\n6. [success] 接下来将重启应用');
+  console.log('6. [success] 接下来将重启应用');
   console.log(`当前位置: ${shell.pwd()}`);
   // 8. 重启
   if (shell.exec('npm run restart:pro').code !== 0) {
-    console.log('\n6. [fail] 重启失败');
+    console.log('6. [fail] 重启失败');
     return false;
   }
 }
