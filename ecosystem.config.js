a78b1fc1192a2de7ad8a842287b26dc19dd8169a
qianyin925 <qianyin95@gmail.com>
2020-03-23 09:40:31 +0800

    refactor: 【server】webhook 服务重新测试编写

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,20 +20,13 @@ module.exports = {
     },
     {
       name: 'cron',  //定时任务
-      watch: ['cron'],
       script: './cron',
       env: {
         "NODE_ENV": "production",
       },
       env_development: {
         "NODE_ENV": "development",
-        watch: [
-          'app',
-          'config',
-          'html',
-          'models',
-          'utils',
-        ],
+        watch: ['cron'],
       }
     }
   ]
