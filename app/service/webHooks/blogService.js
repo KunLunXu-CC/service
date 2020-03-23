081417b684669227899460847abd1798541e2f80
qianyin925 <qianyin95@gmail.com>
2020-03-23 09:58:42 +0800

    refactor: 【server】webhook 服务重新测试编写

--- app/service/webHooks/blogService.js
+++ app/service/webHooks/blogService.js
@@ -26,35 +26,35 @@ module.exports = async ({ body, header }) => {
   }
   console.log('2. [success] 撤销 git 的所有本地修改成功');
 
-  // // 4. 拉取代码
-  // if (shell.exec('git pull').code !== 0) {
-  //   console.log('3. [fail] 拉取代码失败');
-  //   return false;
-  // }
-  // console.log('3. [success] 拉取代码成功');
+  // 4. 拉取代码
+  if (shell.exec('git pull').code !== 0) {
+    console.log('3. [fail] 拉取代码失败');
+    return false;
+  }
+  console.log('3. [success] 拉取代码成功');
 
-  // // 5. 安装依赖
-  // if (
-  //   shell.exec('npm i').code !== 0 &&
-  //   shell.exec('npm i --only=dev').code !== 0
-  // ) {
-  //   console.log('4. [fail] 安装依赖失败');
-  //   return false;
-  // }
-  // console.log('4. [success] 安装依赖成功');
+  // 5. 安装依赖: npm i 安装生产环境下依赖、--only=dev 则是安装开发环境下依赖
+  if (
+    shell.exec('npm i').code !== 0 &&
+    shell.exec('npm i --only=dev').code !== 0
+  ) {
+    console.log('4. [fail] 安装依赖失败');
+    return false;
+  }
+  console.log('4. [success] 安装依赖成功');
 
-  // // 6. 设置权限
-  // shell.chmod('-R', 777, '.');
-  // console.log('5. [success] 设置权限成功');
+  // 6. 设置权限
+  shell.chmod('-R', 777, '.');
+  console.log('5. [success] 设置权限成功');
 
-  // // 7. 提示：完成
-  // console.log(`=======>>>> [webhooks] ${repository.name}: success <<<<=======`);
+  // 7. 提示：完成
+  console.log(`=======>>>> [webhooks] ${repository.name}: success <<<<=======`);
 
-  // console.log('6. [success] 接下来将重启应用');
-  // console.log(`当前位置: ${shell.pwd()}`);
-  // // 8. 重启
-  // if (shell.exec('npm run restart:pro').code !== 0) {
-  //   console.log('6. [fail] 重启失败');
-  //   return false;
-  // }
+  console.log('6. [success] 接下来将重启应用');
+  console.log(`当前位置: ${shell.pwd()}`);
+  // 8. 重启
+  if (shell.exec('npm run restart:pro').code !== 0) {
+    console.log('6. [fail] 重启失败');
+    return false;
+  }
 }
