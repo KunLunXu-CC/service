a5c8855ba25132bf82e6682c4478abb225778b81
qianyin925 <qianyin95@gmail.com>
2020-03-23 09:50:57 +0800

    refactor: 【server】webhook 服务重新测试编写

--- app/service/webHooks/blogService.js
+++ app/service/webHooks/blogService.js
@@ -2,29 +2,29 @@ const path = require('path');
 const shell = require('shelljs');
 
 module.exports = async ({ body, header }) => {
-  // const { repository, ref } = body;
-  // if (ref !== 'refs/heads/master'){
-  //   console.log('[webhooks] 提交非 master 分支代码');
-  //   return false;
-  // }
+  const { repository, ref } = body;
+  if (ref !== 'refs/heads/master'){
+    console.log('[webhooks] 提交非 master 分支代码');
+    return false;
+  }
 
-  // // 1. 提示: 脚本开始
-  // console.log(`=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`);
+  // 1. 提示: 脚本开始
+  console.log(`=======>>>> [webhooks] ${repository.name}: submit new code <<<<=======`);
 
-  // // 2. 进入项目目录
-  // shell.cd(path.resolve(__dirname, '../../../'));
-  // console.log('1. [success] 进入项目目录', shell.pwd());
+  // 2. 进入项目目录
+  shell.cd(path.resolve(__dirname, '../../../'));
+  console.log('1. [success] 进入项目目录', shell.pwd());
 
-  // // 3. 撤销 git 的所有本地修改
-  // if (shell.exec(`
-  //   git fetch --all  && \
-  //   git reset --hard origin/master  && \
-  //   git clean -df
-  // `).code !== 0) {
-  //   console.log('2. [fail] 撤销 git 的所有本地修改失败');
-  //   return false;
-  // }
-  // console.log('2. [success] 撤销 git 的所有本地修改成功');
+  // 3. 撤销 git 的所有本地修改
+  if (shell.exec(`
+    git fetch --all  && \
+    git reset --hard origin/master  && \
+    git clean -df
+  `).code !== 0) {
+    console.log('2. [fail] 撤销 git 的所有本地修改失败');
+    return false;
+  }
+  console.log('2. [success] 撤销 git 的所有本地修改成功');
 
   // // 4. 拉取代码
   // if (shell.exec('git pull').code !== 0) {
