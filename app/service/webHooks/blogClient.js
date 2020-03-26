11888716589e7e8680194c03aed8e38f7a0ed0cf
qianyin925 <qianyin95@gmail.com>
2020-03-26 14:36:11 +0800

    build: webhook 修改

--- app/service/webHooks/blogClient.js
+++ app/service/webHooks/blogClient.js
@@ -34,7 +34,7 @@ module.exports = async ({ body, header }) => {
   console.log('3. [success] 拉取代码成功');
 
   // 5. 安装依赖
-  shell.rm('-rf node_modules');
+  shell.rm('-rf', 'node_modules');
   if (
     shell.exec('npm i').code !== 0 &&
     shell.exec('npm i --only=dev').code !== 0
