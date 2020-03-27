2779bd767a14b4e7e92dbea149b5844fb360eb54
qianyin925 <qianyin95@gmail.com>
2020-03-27 14:10:06 +0800

    feat: 【server】webhook 调整

--- app/route.js
+++ app/route.js
@@ -1,12 +1,5 @@
 const Router = require('koa-router');
-const koaBody = require('koa-body');
 const router = new Router();
-
-// 路由配置
-router.get('/', async (ctx, next) => { ctx.body = '这是一首页'; });
-
-// 微信开发
-router.get('/wechat', require('./wechat'));
 
 // github webhooks
 router.post('/web-hooks', require('./service/webHooks'));
