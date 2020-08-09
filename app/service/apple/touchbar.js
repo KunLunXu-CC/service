6704e9ad7b63ec3d77283c0affbdf2148594fe87
qianyin925 <qianyin925@qq.com>
2020-08-09 15:27:22 +0800

    feat: 【server】添加 appleTouchbar 接口供 touchbar 调用

---
+++ app/service/apple/touchbar.js
@@ -0,0 +1,17 @@
+const _ = require('lodash');
+
+const getLatestWeight = async ({ ctx }) => {
+  const server = ctx.db.mongo.Diary;
+  const [data] = await server.find()
+    .skip(1)
+    .limit(1)
+    .sort({ name: -1 });
+  return _.get(data, 'bodyIndex.weight', 0);
+};
+
+// 用于 mac touchbar 上信息提示
+module.exports = async args => {
+  return {
+    latestWeight: await getLatestWeight(args),
+  };
+};
