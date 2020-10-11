316ac597e5bc4013e2d3ef479f01beecea1c3bbf
qianyin925 <qianyin925@qq.com>
2020-10-11 10:45:28 +0800

    refactor: 【serve】Article 模型调整、以及对应接口调整

--- scripts/tmp/index.script.js
+++ scripts/tmp/index.script.js
@@ -1,44 +1,14 @@
 // 临时脚本
 const mongo = require('../../utils/mongo');
-const dbData = require('./db.json');
-const { STATUS } = require('../../config/consts');
-
-// 插入字典数据
-const insertDatasetsfrom = async db => {
-  const admin = await db.User.findOne({
-    account: 'admin',
-    status: { $ne: STATUS.DELETE },
-  });
-  await db.Datasetsfrom.insertMany(dbData.map(v => ({
-    ...v,
-    creator: admin.id,
-    updater: admin.id,
-  })));
-  console.log('------ 插入字典数据成功 --------');
-};
-
-// 处理日志数据
-const handleDiary = async db => {
-  const data = await db.Diary.find();
-  for (let item of data) {
-    const { fitness, id } = item;
-
-    id && fitness && fitness.length > 0 && await db.Diary.updateMany(
-      { _id: item.id },
-      { fitness: fitness.map(v => ({
-        type: v.type,
-        place: v.place,
-      }))}
-    );
-  }
-  console.log('------ 处理日志数据成功 --------');
-}
 
 module.exports = {
   exec: async () => {
     const db = mongo();
-    await insertDatasetsfrom(db);
-    await handleDiary(db);
+    await db.Article.updateMany({}, {
+      type: 0,
+      tags: [],
+    }, {});
+    console.log('\n\n\n修改文章类型成功!\n\n\n\n');
   },
   name: '临时脚本',
 };
