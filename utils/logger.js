fbe550131f946a39437554ff8ef84c25e5d406c3
qianyin925 <qianyin925@qq.com>
2020-10-27 15:32:38 +0800

    refactor: 【server】node 日志输出控制

--- utils/logger.js
+++ utils/logger.js
@@ -1,36 +1,37 @@
 const _ = require('lodash');
 const path = require('path');
-const { resume_up } = require('qiniu');
 const winston = require('winston');
 require('winston-daily-rotate-file');
 
 // 文件
 const fileTransports = new winston.transports.DailyRotateFile({
-  // // level: 'info',
-  format: winston.format.printf(info => {
-    try {
-      return JSON.stringify(info);
-    } catch {
-    }
-  }),
   filename: path.resolve(__dirname, '../logs/%DATE%.log'),
-    // datePattern: 'YYYY-MM-DD-HH',
-    // // zippedArchive: true,
-    // maxSize: '20m',
-    // maxFiles: '14d'
     datePattern: 'YYYY-MM-DD-HH',
-    zippedArchive: false,
-    maxSize: '20m',
     maxFiles: '14d',
+    maxSize: '20m',
 });
 
 // 控制台
-const consoleTransports = new winston.transports.Console();
+const consoleTransports = new winston.transports.Console({
+});
+
+// 自定义
+const customTransport = new class extends winston.Transport {
+  constructor(opts) {
+    super(opts);
+  }
+
+  log(info, callback) {
+    console.log('info\n\n\n\n\n',info);
+    callback()
+  }
+}
 
 module.exports = winston.createLogger({
+  format: winston.format.printf(info =>info.message),
   transports: [
-    consoleTransports,
     fileTransports,
-    // new winston.transports.File({filename: 'combined.log'})
+    customTransport,
+    consoleTransports,
   ]
 });
