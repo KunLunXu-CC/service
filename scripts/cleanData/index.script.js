9f7d35a5ea835580d9b1085262b28d1b414bdc42
qianyin925 <qianyin925@qq.com>
2020-11-21 10:21:57 +0800

    refactor: 【client】日志实时查看 - 前后端打通

--- scripts/cleanData/index.script.js
+++ scripts/cleanData/index.script.js
@@ -3,20 +3,14 @@ const path = require('path');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
 
-const mongo = require('../../utils/mongo');
+const db = require('../../utils/mongo');
 const { requireFiles, delayed } = require('../../utils');
 const { STATUS } = require('../../config/consts');
 
 module.exports = {
   name: '清理数据(删除假删数据)',
   exec: async () => {
-    // 1. 连接数据库
-    const db = mongo();
-    const spinner = ora('连接数据库中……').start();
-    await delayed(1000 * 10);
-    spinner.stop();
-
-    // 2. 读取表, 并选择要清除的表
+    // 1. 读取表, 并选择要清除的表
     const files = requireFiles({
       dir: path.resolve(__dirname, '../../models'),
     });
@@ -32,7 +26,7 @@ module.exports = {
       },
     ]);
 
-    // 3. 删除数据
+    // 2. 删除数据
     for(let key of dbList){
       await db[key].remove({ status: STATUS.DELETE });
     }
