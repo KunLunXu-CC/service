ea9473ded15fd79efe2aaa047539412b43195208
qianyin925 <qianyin925@qq.com>
2020-11-21 10:49:11 +0800

    refactor: 【client】日志实时查看 - 前后端打通

--- app/ws/logger/index.js
+++ app/ws/logger/index.js
@@ -1,31 +1,17 @@
 const WebSocket = require('ws');
-const mongo = require('../../../utils/mongo');
+const db = require('../../../utils/mongo');
+const { APP_CODE } = require('../../../config/consts');
 const { verifyJwt } = require('../../../utils/encryption');
 
 const wss = new WebSocket.Server({ noServer: true });
 
-/**
- * 校验权限
- * @param {*} jwt json web token
- * @return {Boolean} 是否具有权限
- */
-const verifyPermissions = async jwt => {
-  const data = await verifyJwt('jwt');
-  console.log((await verifyJwt('jwt')).role, '1data');
-  console.log((await verifyJwt(jwt)).role, '1data');
-
-  // db.Role.find({ _id:  });
-
-  // const { protocol } = ws;
-  // const data = verifyJwt(protocol);
-  // console.log('data1111111', protocol, data);
-};
-
-wss.on('connection', function connection(ws) {
-  verifyPermissions(ws.protocol);
-  // TODO: 身份校验, 通过携带的 protocol(JWT)
-
-  // ws.close();
+wss.on('connection', connection = async ws => {
+  // 1. 对 ws.protocol(JWT) 进行校验
+  const data = await verifyJwt(ws.protocol);
+  // 2. 获取角色信息
+  const role = data.role ? await db.Role.findOne({ _id: data.role }) : {};
+  // 3. 判断当前角色是否具有权限, 没有则关闭连接
+  !(role.auth || []).find(v => v.code === APP_CODE.LOGGER) && ws.close();
 });
 
 module.exports = wss;
