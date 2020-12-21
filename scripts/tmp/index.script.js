a04d2cd44a252f9c58c4def6986a892092e1d08c
qianyin925 <qianyin925@qq.com>
2020-12-21 09:05:06 +0800

    refactor: 修改临时脚本

--- scripts/tmp/index.script.js
+++ scripts/tmp/index.script.js
@@ -3,11 +3,21 @@ const db = require('../../utils/mongo');
 
 module.exports = {
   exec: async () => {
-    await db.Article.updateMany({}, {
-      type: 0,
-      tags: [],
-    }, {});
-    console.log('\n\n\n修改文章类型成功!\n\n\n\n');
+
+    const data = await db.Role.find();
+    for (let item of data) {
+      const auth = item.auth.map(v => {
+        v.code === 'read' && (v.code = 'reader');
+        return v;
+      });
+
+      await db.Role.updateMany(
+        { _id: item.id },
+        { auth }
+      );
+    }
+
+    console.log('\n\n\n脚本执行成功!\n\n\n\n');
   },
   name: '临时脚本',
 };
