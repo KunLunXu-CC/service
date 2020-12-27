59fbceb49d050a15d901830273148285b27f1c5c
qianyin925 <qianyin925@qq.com>
2020-12-27 10:20:24 +0800

    ci: 临时脚本更新

--- scripts/tmp/index.script.js
+++ scripts/tmp/index.script.js
@@ -3,20 +3,13 @@ const db = require('../../utils/mongo');
 
 module.exports = {
   exec: async () => {
-
-    const data = await db.Role.find();
+    const data = await db.Article.find();
     for (let item of data) {
-      const auth = item.auth.map(v => {
-        v.code === 'read' && (v.code = 'reader');
-        return v;
-      });
-
-      await db.Role.updateMany(
+      item.type === -99999 && await db.Article.updateMany(
         { _id: item.id },
-        { auth }
+        { type: 0 }
       );
     }
-
     console.log('\n\n\n脚本执行成功!\n\n\n\n');
   },
   name: '临时脚本',
