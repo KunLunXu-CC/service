7f285ddd9cd986fd36cdcacfcd7638941caf64fe
qianyin925 <qianyin925@qq.com>
2021-04-05 19:08:43 +0800

    feat: 定时任务调整

--- cron/index.js
+++ cron/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const CronJob = require('cron').CronJob;
 const { requireFiles } = require('../utils');
 
-// 获取当前目录下所有定时任务
+// 获取当前目录下所有定时任务(加载所有 index.cron.js 文件)
 const getCrons = (crons = []) => {
   // 1. 加载所有脚本配置: [{ cronTime, onTick, onComplete}]
   requireFiles({
@@ -17,7 +17,12 @@ const getCrons = (crons = []) => {
   return crons;
 }
 
-// 创建定时任务
+/**
+ * 创建定时任务, 定时任务配置:
+ * 1. cronTime 时间模式, 和 linux 下定时任务一样
+ * 2. onComplete 当通过停止作业时将触发的函数(没有则设置为 null)
+ * 3. onTick 定时任务
+ */
 _.values(getCrons()).map( setting => new CronJob(
   setting.cronTime,
   setting.onTick,
