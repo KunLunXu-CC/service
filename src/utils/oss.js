4ce31134d31d64984aa1107aef946245087b5e42
qianyin925 <qianyin925@qq.com>
2023-03-01 21:44:55 +0800

    feat: 🎸 add oss

---
+++ src/utils/oss.js
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import path from 'path';
+import OSS from 'ali-oss';
+import system from '#config/system';
+
+const client = new OSS(system.oss);
+
+/**
+ * 处理文件名(要存入数据库的文件名)
+ *
+ * @param {string} sourceFileName 上传文件的源文件名
+ * @returns {string} 返回文件名: klx. {当前环境}.{原文件名+时间戳的 base64 }.{文件后缀}
+ */
+const getFileName = (sourceFileName) => {
+  const extname = path.extname(sourceFileName);
+  const unique = `${sourceFileName}${new Date().getTime()}`;
+  const name = Buffer.from(unique).toString('base64');
+  const env = process.env.NODE_ENV === 'development' ? 'dev' : 'pro';
+  return `klx.${env}.${name}${extname}`;
+};
+
+// 文件上传
+export const upload = async ({ fileName, fileStream, filePath }) => {
+  const stream = fileStream
+    ? fileStream
+    : fs.createReadStream(filePath);
+  const result = await client.putStream(getFileName(fileName), stream);
+  return result;
+};
+
+export const span = null;
+
+// const result = await client.put('exampleobject.txt', path.normalize(new URL('./qiniu.js', import.meta.url).pathname));
+
+const res = await upload({
+  fileName: 'exampleobject.txt',
+  filePath: new URL('./qiniu.js', import.meta.url).pathname,
+});
+
+console.log('%c [ res ]-41', 'font-size:13px; background:pink; color:#bf2c9f;', res);
