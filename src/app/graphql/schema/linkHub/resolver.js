b4a83dfd57427281792af88b198e725aa9157329
MoYuanJun <moyuanjun925@gmail.com>
2026-04-23 10:46:13 +0800

    chore: update linkHub

--- src/app/graphql/schema/linkHub/resolver.js
+++ src/app/graphql/schema/linkHub/resolver.js
@@ -1,6 +1,6 @@
 import create from '#service/common/create';
-// import remove from '#service/common/remove';
-// import update from '#service/common/update';
+import remove from '#service/common/remove';
+import update from '#service/common/update';
 import getList from '#service/common/getList';
 
 export default {
@@ -11,36 +11,31 @@ export default {
         model: 'LinkHub',
         ctx: context.ctx,
       });
-      console.log('%c [ list ]-15', 'font-size:13px; background:pink; color:#bf2c9f;', list);
       return {
         list,
         pagination,
       };
     },
 
-    // tagsWithArticles: async (parents, args, context) => await tagsWithArticles({
-    //   ...args,
-    //   ctx: context.ctx,
-    // }),
   },
 
   Mutation: {
+    removeLinkHub: async (parents, args, context) => await remove({
+      ...args,
+      model: 'LinkHub',
+      ctx: context.ctx,
+    }),
+
     createLinkHub: async (parents, args, context) => await create({
       ...args,
       model: 'LinkHub',
       ctx: context.ctx,
     }),
 
-    // removeTags: async (parents, args, context) => await remove({
-    //   ...args,
-    //   model: 'Tag',
-    //   ctx: context.ctx,
-    // }),
-
-    // updateTags: async (parents, args, context) => await update({
-    //   ...args,
-    //   model: 'Tag',
-    //   ctx: context.ctx,
-    // }),
+    updateLinkHub: async (parents, args, context) => await update({
+      ...args,
+      model: 'LinkHub',
+      ctx: context.ctx,
+    }),
   },
 };
